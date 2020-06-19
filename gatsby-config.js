9571d7dce90a34b20d8b9082a9bd0dcce2dac2bc
semigura <shotaro-ishigaki@gaji.jp>
2020-06-19 13:38:37 +0900

    make offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -175,17 +175,6 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-      },
-    },
     'gatsby-plugin-offline'
   ],
 };

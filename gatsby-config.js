1cd1828c12c0e15f5c3315a671c3f65de9532ba8
semigura <shotaro-ishigaki@gaji.jp>
2020-06-19 13:33:05 +0900

    tmp

--- gatsby-config.js
+++ gatsby-config.js
@@ -175,5 +175,17 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    'gatsby-plugin-offline'
   ],
 };
